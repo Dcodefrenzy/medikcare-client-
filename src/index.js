b9838fc98cc7808f9e1b89b309a841a59cfe25ff
Dcodefrenzy <ayodejisamuelfakunle@gmail.com>
2020-02-09 18:20:04 -0800

    final chat push

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import * as serviceWorker from './serviceWorker';
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
 
 
 ReactDOM.render(
