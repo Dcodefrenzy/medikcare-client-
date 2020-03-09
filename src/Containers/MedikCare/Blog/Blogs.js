0ae4fc816c2ad75b39a4fae7a688181a78cf42ab
Dcodefrenzy <ayodejisamuelfakunle@gmail.com>
2020-03-09 05:04:32 -0700

    design client blog page

--- src/Containers/MedikCare/Blog/Blogs.js
+++ src/Containers/MedikCare/Blog/Blogs.js
@@ -12,39 +12,42 @@ const Blog = (props)=>{
                 <div className="col-12 col-sm-12 col-md-12 top-margin-lg">
                     <div className="row">
                         <div className="col-12 col-sm-12 col-md-4  top-margin-sm">
-                            <div className="card">
-                                <img src="Images/user.png" className="img-thumbnail card opacity-8 bg-dark" />
-                                <div className="card-body">
-                                <h1>Health Care In Nigeria</h1>
-                                <span>Kolade Fakunle</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-12 col-sm-12 col-md-4  top-margin-sm">
-                            <div className="card">
-                                <img src="Images/user.png" className="img-thumbnail card opacity-8 bg-dark" />
-                                <div className="card-body">
+                            <div className="card z-index bg-dark">
+                                <img src="Images/user.png" className="blog-position img-thumbnail card opacity-5 bg-dark" />
+                                <div className="card-body blog-position-text text-white">
                                 <h1>Health Care In Nigeria</h1>
                                 <span><i className="card-text fa fa-user"></i> Kolade Fakunle</span>
-                                <span className="card-text">20 mins ago</span>
+                                <span className="float-right text-success">20 mins ago</span>
                                 </div>
                             </div>
                         </div>
                         <div className="col-12 col-sm-12 col-md-4  top-margin-sm">
-                            <div className="card">
-                                <img src="Images/user.png" className="img-thumbnail card opacity-8 bg-dark" />
-                                <div className="card-body">
+                            <div className="card z-index bg-dark">
+                                <img src="Images/user.png" className="blog-position img-thumbnail card opacity-5 bg-dark" />
+                                <div className="card-body blog-position-text text-white">
                                 <h1>Health Care In Nigeria</h1>
-                                <span>Kolade Fakunle</span>
+                                <span><i className="card-text fa fa-user"></i> Kolade Fakunle</span>
+                                <span className="float-right text-success">20 mins ago</span>
                                 </div>
                             </div>
                         </div>
                         <div className="col-12 col-sm-12 col-md-4  top-margin-sm">
-                            <div className="card">
-                                <img src="Images/user.png" className="img-thumbnail card opacity-8 bg-dark" />
-                                <div className="card-body">
+                            <div className="card z-index bg-dark">
+                                <img src="Images/user.png" className="blog-position img-thumbnail card opacity-5 bg-dark" />
+                                <div className="card-body blog-position-text text-white">
                                 <h1>Health Care In Nigeria</h1>
-                                <span>Kolade Fakunle</span>
+                                <span><i className="card-text fa fa-user"></i> Kolade Fakunle</span>
+                                <span className="float-right text-success">20 mins ago</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-12 col-sm-12 col-md-4  top-margin-sm">
+                            <div className="card z-index bg-dark text-white">
+                                <img src="Images/user.png" className="blog-position img-thumbnail card opacity-5 bg-dark" />
+                                <div className="card-body blog-position-text text-white">
+                                <h1>Health Care In Nigeria</h1>
+                                <span><i className="card-text fa fa-user"></i> Kolade Fakunle</span>
+                                <span className="float-right text-success">20 mins ago</span>
                                 </div>
                             </div>
                         </div>
