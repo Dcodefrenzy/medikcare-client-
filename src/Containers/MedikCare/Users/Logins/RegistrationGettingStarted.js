1f4872218519e4e2f7bf8b14b89d3ca9089a8208
Dcodefrenzy <ayodejisamuelfakunle@gmail.com>
2020-03-11 02:30:36 -0700

    connected the blog with the backend feature

--- src/Containers/MedikCare/Users/Logins/RegistrationGettingStarted.js
+++ src/Containers/MedikCare/Users/Logins/RegistrationGettingStarted.js
@@ -9,18 +9,18 @@ import Time  from '../../Assets/svgs/Health_Remotely.svg'
 const RegistrationGettingStarted = (props) => {
     let gettingStartedMessage = 
     [
-        {"bgColor":"bg-warning","headerText":"ONLINE CONSULTATION","textMessage":"We save you the stress of going to the hospital to see doctor. You can now consult with a doctor virtually on out platform","svg":Time},
-        {"bgColor":"b-medik","textColor":"text-white","headerText":"TREATMENT PLAN","textMessage":"We'll pair you with a doctor that best meets your needs, and they will give you a treatment plan and next steps to put you or your family member on the road to recovery.","svg":Doctor},
-        {"bgColor":"bg-success","headerText":"PAY A TOKEN","textMessage":"We won't give you unnecessary bills, you will only pay a token of 300 naira to consult with our doctors.","svg":Payment},
-        {"bgColor":"bg-chocolate","headerText":"ASK HEALTH QUESTIONS FOR FREE","textMessage":"You can ask our pool of doctors questions for free and you will get answers in 12 hours or less.","svg":Article}
+       {"bgColor":"b-medik","textColor":"text-white","headerText":"TREATMENT PLAN","textMessage":"We'll pair you with a doctor that best meets your needs.","svg":Doctor},
+        {"bgColor":"bg-success","headerText":"PAY A TOKEN","textMessage":"You will only pay a token of 300 naira to consult with our doctors.","svg":Payment},
+        {"bgColor":"bg-chocolate","headerText":"ASK HEALTH QUESTIONS FOR FREE","textMessage":"Ask health questions for free and you will get answers quickly.","svg":Article}
     ]
     const [count, setCounter] =useState(0)
     const [display, setDisplay] =useState({display:"container"})
-   const [gettingStarted, setGettingStarted] = useState({"bgColor":"bg-warning","headerText":"ONLINE CONSULTATION","textMessage":"We save you the stress of going to the hospital to see doctor. You can now consult with a doctor virtually on out platform","svg":Time});
+   const [gettingStarted, setGettingStarted] = useState({"bgColor":"bg-warning","headerText":"ONLINE CONSULTATION","textMessage":"You can now consult with a doctor online on out platform","svg":Time});
 
 const moveLeft = (event, count)=>{
         if(count === 0){   
-        setGettingStarted(gettingStartedMessage[count]);
+        setGettingStarted( {"bgColor":"bg-warning","headerText":"ONLINE CONSULTATION","textMessage":"You can now consult with a doctor online on out platform","svg":Time},
+        );
         }else{
             
         const newCount = count -1;        
@@ -70,21 +70,21 @@ const hideDisplay = (event)=>{
                 <div className={`card opacity-9 fixed  ${gettingStarted.bgColor}`}>
                 </div>
             </div>
-            <div className={`col-12 col-sm-12 col-md-12 col-lg-6 offset-lg-3 fixed top-margin-lg`}>
+            <div className={`col-12 col-sm-12 col-md-12 col-lg-6 offset-lg-3 fixed top-margin-sm`}>
                 <main className="opacity">
                     <div className="row align-center">
                         <div className="col-12  col-sm-12 col-md-6 "> 
                             <img  src={gettingStarted.svg} alt="Login on medikcare platform" className="text-white home-svg"/>
                         </div>
-                        <div className="col-12 col-sm-12 col-md-6 top-margin-sm">
+                        <div className="col-12 col-sm-12 col-md-6">
                         <h1 className="text-dark">{gettingStarted.headerText}</h1>
                         <h5 className="text-white">{gettingStarted.textMessage}</h5>
                         </div>
                     </div>
                     <div className="row">
-                        <div className="col-12  col-sm-12 col-md-12 top-margin-sm">
-                            <i onClick={event=>moveLeft(event, count)} className="fa fa-arrow-circle-left"></i>
-                            <i onClick={event=>moveRight(event, count)} className="fa fa-arrow-circle-right float-right"></i>
+                        <div className="col-12  col-sm-12 col-md-12">
+                            <i onClick={event=>moveLeft(event, count)} className="fa fa-arrow-circle-left fa-xs"></i>
+                            <i onClick={event=>moveRight(event, count)} className="fa fa-arrow-circle-right fa-xs float-right"></i>
                         </div>
                     </div>
                     <div className="row align-center">
--- src/Containers/MedikCare/Users/Profile/Profile.js
+++ src/Containers/MedikCare/Users/Profile/Profile.js
@@ -170,7 +170,7 @@ const userProfile = (props) => {
                                     <div className="col-12 col-sm-12 col-md-12">
                                         <div className="row">
                                            <div className="col-5 col-sm-5 col-md-5">
-                                           <img className="img-thumbnail" width="100%" src={"/Images/"+file.filename} alt="admin-profile-image"/>
+                                           <img className="img-thumbnail" width="100%" src={process.env.PUBLIC_URL+"/Images/"+file.filename} alt="admin-profile-image"/>
                                            <Link to="/user/image/update">
                                                 <small className="display-block">Click here to add a profile picture</small>
                                            </Link>
