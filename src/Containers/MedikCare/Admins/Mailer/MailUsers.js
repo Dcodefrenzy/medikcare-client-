5992bcb3620308ee7d7310194e5b6f59daeb441b
Dcodefrenzy <ayodejisamuelfakunle@gmail.com>
2020-03-16 09:58:19 -0700

    add blog link to blog admin panel

--- src/Containers/MedikCare/Admins/Mailer/MailUsers.js
+++ src/Containers/MedikCare/Admins/Mailer/MailUsers.js
@@ -17,18 +17,46 @@ const [usersMail, setUsersMail] = useState({value:"", display:"display-none"})
 
 
 const externalStateHandler = (event, result)=>{
-        if (result === true) {
-            
-        setUsersMail({value:event.target.value, display:"display-none"})
-        }else{
-        setUsersMail({value:event.target.value, display:""})
-        }
-        setExternalUsers({bool:!result});
+    if (result === true) {
+        
+    setUsersMail({value:event.target.value, display:"display-none"})
+    }else{
+    setUsersMail({value:event.target.value, display:""})
+    }
+    setExternalUsers({bool:!result});
 
 }
 const validateUsersMail = (event)=>{
-    setUsersMail({value:event.target.value, display:""})
+setUsersMail({value:event.target.value, display:""})
 }
+const mailUsers=(event)=>{
+    event.preventDefault();
+    let url
+    const mailler = {externalUsers:usersMail.value};
+    if (externalUsers.bool === true) {
+         url = "/api/v1/mailler/notify-external-users/"+props.id;
+    }else{
+         url = "/api/v1/mailler/notify-users/"+props.id;
+    }
+    console.log(mailler)
+    fetch(url, {
+        method:"POST",
+        body:JSON.stringify(mailler),
+        headers:{'Content-Type': "application/json", "x-auth": sessionItem.token}
+    })
+    .then(res=>res.json())
+    .then(response=>{
+        if (response.status === 201) {
+            console.log(response)
+            window.location ="/admin/mail/";
+        }else{
+            console.log(response)
+        }
+    })
+}
+
+
+
 
 
 
@@ -47,7 +75,7 @@ const validateUsersMail = (event)=>{
                             <i onClick={props.click}  className="float-right fa fa-times" arialhidden="true"></i>  
                                     <h1>Blog Images</h1>
                                     <span>please upload a blog image.</span> 
-                                    <form>
+                                    <form onSubmit={mailUsers}>
                                     <div className="row">                                        
                                         <div className="col-12 col-sm-12 col-md-12">
                                             <div className="form-group">
