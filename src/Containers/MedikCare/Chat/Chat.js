31a42645bca839efd56427a43048597f1199fbc5
Dcodefrenzy <ayodejisamuelfakunle@gmail.com>
2020-05-02 13:29:12 +0100

    push changes

--- src/Containers/MedikCare/Chat/Chat.js
+++ src/Containers/MedikCare/Chat/Chat.js
@@ -3,89 +3,29 @@ import Moment from 'react-moment';
 import 'moment-timezone';
 import { Link } from 'react-router-dom';
 import {socket} from '../Socket/Socket';
+import ChatForm from "./ChatForm";
  
 const Chat =(props)=>{
     const to  = props.match.params.id
  
     let dashboardLink;
     let userUrl;
-    let medikForm;
     let isUserActive;
     let isDoctorActive;
     let sess;
+    let msg = "";
     const sessionItemUser = JSON.parse(sessionStorage.getItem("user"));
-    const sessionItemDoctor = JSON.parse(sessionStorage.getItem("doctor"));
-    const [message, setMessage] = useState({id:"msg", value:"", type:"text"}) 
-    const [userMessage, setUserMessage]=useState({id:"msg", value:"", type:"text"});
-    const [doctorMessage, setDoctorMessage]=useState({id:"msg", value:"", type:"text"});
+    const sessionItemDoctor = JSON.parse(sessionStorage.getItem("doctor")); 
     const [messages, setDisplayMessage] = useState([]) 
-    const [notifyMessages, setDisplayNotifyMessage] = useState({ value:""}) 
     const [scroll, setScroll] = useState({id:"scroll"}) 
     const [userDetail,setUserDetail] = useState({});
     const [room, setRoomSession] = useState({roomSession:null});
     const [session, setSession] = useState({});
+    const [typingUser, setTypingUser] = useState({value:""})
 
     const element = useRef(null);
-
-    
-    const sessionHandller=()=>{
-        if(sessionItemUser === null && sessionItemDoctor !== null){
-            setSession({sessionItemDoctor})
-                getUserDetailsHandller();
-           
-                fetchChatMessage();
-
-       }else if(sessionItemUser !== null && sessionItemDoctor === null) {
-           setSession(sessionItemUser)
-           getUserDetailsHandller();
-           
-       fetchChatMessage();
-       }else{
-           console.log("err")
-       }
-    }
-    const allowPush=()=>{
-        const OneSignal = window.OneSignal || []; 
-            
-      //  OneSignal.showSlidedownPrompt();
-      
-			OneSignal.push(function() {
-				// Occurs when the user's subscription changes to a new value.
-				OneSignal.on('subscriptionChange', function (isSubscribed) {
-                    if (isSubscribed) {
-                        OneSignal.getUserId().then(function(userId) {
-                            let url;
-                            if (sessionItemUser === null && sessionItemDoctor !== null) {
-                            url = "/api/v1/doctor/update/notification/"+userId
-                            }else if (sessionItemUser !== null && sessionItemDoctor === null) {
-                                url = "/api/v1/user/update/notification/"+userId
-                            }
-                        if (userId) {	
-                            fetch(url, {
-                                method: "PATCH",
-                                headers: {'Content-Type': "application/json", "u-auth": session.token}
-                            })
-                            .then(res => res.json())
-                            .then(response => { 
-                                if(response.status === 401) {
-                                sessionStorage.removeItem("user");
-                                    window.location = "/login?Session expired please login."
-                                }else if (response.status === 200) {
-                                    
-                                }
-                            })
-                            }
-                            
-                        
-                        })
-                    }
-				  console.log("The user's subscription state is now:", isSubscribed);
-				});
-				
-				// This event can be listened to via the `on()` or `once()` listener.
-			  });
-    }
-
+    const textElement = useRef(null);
+    const typingRef = useRef("");
 
     if (sessionItemUser === null && sessionItemDoctor !== null) {
           dashboardLink ="/chat/doctors/doctor";
@@ -98,10 +38,8 @@ const Chat =(props)=>{
          sess = sessionItemUser;
          isUserActive = true;
    }
-    const scrollToBottom = ()=>{
-          
+    const scrollToBottom = ()=>{  
         element.current.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
-     
     }
 
     const getUserDetailsHandller = ()=>{
@@ -120,7 +58,6 @@ const Chat =(props)=>{
     }
 
     const notify = (data,url)=>{
-        console.log(url)
         fetch(url, {
             method:"POST",
             body:JSON.stringify(data),
@@ -143,21 +80,12 @@ const Chat =(props)=>{
             }
         })
     }
-
-    const setMessageHandler =(event)=>{
-        setMessage({id:"msg", value:event.target.value, type:"text"});
-    }
-
-
     
-    //const socket = io(port, {transports: ['websocket']});
-    const submitChatMessage=(event, )=>{
+    const submitChatMessage=(event)=>{
         event.preventDefault();
         let messageData ={};
-        messageData = {"message":message.value, "from":sess._id, "to":to, "room":room.roomSession};
-        
-        setMessage({id:"msg", value:"", type:"text"})
-        setDisplayNotifyMessage({value:message.value})
+        messageData = {"message":textElement.current.value, "from":sess._id, "to":to, "room":room.roomSession};
+        textElement.current.value = "";
          socket.emit("send message", messageData);
        setDisplayMessage(messages => messages.concat({_id:Date.now(), delivery:true, message:messageData.message,createdAt:Date.now(),from:sess._id}));
         scrollToBottom();
@@ -168,49 +96,30 @@ const Chat =(props)=>{
     const fetchChatMessage =()=>{       
        const messageData = {"from":sess._id, "to":to};  
         socket.emit("fetch message", messageData);
+
+        socket.on("fetch message",(dataset, roomSession)=>{
+            if (dataset === false && !sessionItemUser) {
+                setDisplayMessage([]);
+                console.log(session)
+                window.location = "/chat/doctors/doctor";
+            }else if (dataset === false && !sessionItemDoctor) {
+                setDisplayMessage([]);
+                console.log(session)
+                window.location = "/chat/doctors";
+            }else if (dataset === false && !sessionItemUser && !sessionItemDoctor) {
+                window.location = "/";
+         }else{
+            
+            setRoomSession({roomSession:roomSession});
+            setDisplayMessage(dataset);
+            scrollToBottom();
+            }
+        })
     }
 
-    socket.on("fetch message",(dataset, roomSession)=>{
-        if (dataset === false && !sessionItemUser) {
-            setDisplayMessage([]);
-            console.log(session)
-            window.location = "/chat/doctors/doctor";
-        }else if (dataset === false && !sessionItemDoctor) {
-            setDisplayMessage([]);
-            console.log(session)
-            window.location = "/chat/doctors";
-        }else if (dataset === false && !sessionItemUser && !sessionItemDoctor) {
-            window.location = "/";
-     }else{
-        
-        setRoomSession({roomSession:roomSession});
-        setDisplayMessage(dataset);
-        scrollToBottom();
-        }
-    })
+ 
 
-const viewProfile= (event, id)=>{
-    event.preventDefault();
-    if (sessionItemDoctor) {
-        window.location = "/chat/user/profile/"+id;
-    }else if(sessionItemUser){
-        window.location = "/chat/doctor/profile/"+id;
-    }
-}
-    const endSession=(event, id)=>{
-        event.preventDefault();
-        //console.log(session)
-        if (sessionItemDoctor) {
-            window.location = "/chat/report/"+id;
-        }else if(sessionItemUser){
-            window.location = "/chat/feedback/"+id;
-        }
-    }
-
-
-    useEffect(()=>{
-        sessionHandller();
-        allowPush();
+    const getChatMessage=()=>{
         socket.on("get message",(dataset)=>{
             if (dataset === false && !sessionItemUser) {
                 setDisplayMessage([]);
@@ -225,10 +134,10 @@ const viewProfile= (event, id)=>{
                let messageData = {"message": dataset.message, "from":sess._id, "to":props.match.params.id}; 
     
                if(sessionItemUser !== null && dataset.from === sessionItemUser._id) {
-                console.log("doc")
+               // console.log("doc")
                 notify(messageData, "/api/v1/doctor/notify-doctor");
             }else if( sessionItemDoctor !== null && dataset.from === sessionItemDoctor._id) {
-                console.log("user")
+               // console.log("user")
                 notify(messageData, "/api/v1/user/notify-user");
             }
 
@@ -239,7 +148,48 @@ const viewProfile= (event, id)=>{
             }
             scrollToBottom();
          })
-     
+    }
+
+    const whoIsTyping = (event) =>{
+        socket.emit('typing', room.roomSession);
+
+        socket.on('typing', (message)=>{
+            typingRef.current.value = userDetail.name+" "+message;
+            setTypingUser({value:userDetail.name+" "+message})
+        })
+    }
+    const notTyping=(event)=>{
+        socket.emit('not typing', room.roomSession);
+
+        socket.on('not typing', message=>{
+            setTypingUser({value:""})
+        })
+    }
+
+const viewProfile= (event, id)=>{
+    event.preventDefault();
+    if (sessionItemDoctor) {
+        window.location = "/chat/user/profile/"+id;
+    }else if(sessionItemUser){
+        window.location = "/chat/doctor/profile/"+id;
+    }
+}
+    const endSession=(event, id)=>{
+        event.preventDefault();
+        if (sessionItemDoctor) {
+            window.location = "/chat/report/"+id;
+        }else if(sessionItemUser){
+            window.location = "/chat/feedback/"+id;
+        }
+    }
+
+
+    useEffect(()=>{
+        getUserDetailsHandller();
+        fetchChatMessage();
+        getChatMessage();  
+        whoIsTyping();
+        notTyping();
     }, []);
 
 
@@ -324,18 +274,21 @@ const viewProfile= (event, id)=>{
                                 <div className="clearfix bottom-padding-lg top-padding-md" id={scroll.scroll}></div>
                                 <div className="card b-medik position-fixed  chat-static-buttom">
                                    <div className="card-body">
+                                        <div className="text-white" ref={typingRef} value="www"/>
+                                        <i className="text-white">{typingUser.value}</i>
                                         <form onSubmit={submitChatMessage}>
                                             <div className="row">
-                                               <div className="col-8">
-                                                    <div className="form-group">
-                                                    <textarea name="chat" onChange={event =>setMessageHandler(event, message.id)} id={message.id} value={message.value} className="form-control chat-message" placeholder="Write a message" rows="1" required></textarea> 
-                                                    </div>
-                                               </div>
-                                               <div className="col-4">
-                                                   <input type="submit" className="form-contol btn btn-lg btn-dark" value="Send" />
-                                                   </div>
+                                                <div className="col-8">
+                                                    <div className="form-group"> 
+                                                        <textarea name="chat" onKeyUp={event=>notTyping(event)} onKeyPress={event=>whoIsTyping(event)} ref={textElement} className="form-control chat-message" placeholder="Write a message" rows="1" required></textarea>
+                                                        </div>
+                                                </div>
+                                                <div className="col-4">
+                                                    <input type="submit" className="form-contol btn btn-lg btn-dark" value="Send" />
+                                                </div>
                                             </div>
                                         </form>
+                                       
                                    </div>
                                 </div>
                             </div>
